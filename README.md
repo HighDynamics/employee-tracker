50d2bb12551c48304ea91405188de86cb0f06f0f
Daniel Denton <danielryandenton@gmail.com>
2021-12-08 21:47:05 -0600

    edit readme, minor bug fix

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# employee-tracker
+# Employee-Tracker
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Description
+
+Organize your team with a relational database.
+
+- Add and delete departments, roles, and employees
+- Update employees
+- View department budgets, all roles, and all employees
+
+## Table of Contents
+
+1. [Installation](#Installation)
+
+2. [Usage](#Usage)
+
+3. [Questions](#Questions)
+
+4. [License](#License)
+
+## Installation
+
+Install Packages:
+
+    npm install
+
+Update `connection.js` with mySQL credentials:
+
+    const db = mysql.createPool({
+    host: 'localhost',
+    user: <your-user-name>,
+    password: <your-password>,
+    database: 'employee_tracker',
+    },
+
+SQL statement:
+
+    CREATE DATABASE employee_tracker
+
+## Usage
+
+`node index.js` to run from the command line. Follow the prompts to interact with the database.
+
+## Questions
+
+GitHub profile: [HighDynamics](https://github.com/HighDynamics)  
+Reach out with additional questions at <HighDynamics@gmail.com>
+
+## License
+
+Licensed under [MIT](https://opensource.org/licenses/MIT).
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,27 +1,29 @@
 INSERT INTO departments (name)
 VALUES
-  ('Front-End'), ('Back-End'), ('Quality Assurance');
+  ('Front-End'), ('Back-End'), ('Quality Assurance'), ('Human Resources');
 
 INSERT INTO roles (title, salary, department_id)
 VALUES
   ('Manager', 100000.50, 1),
   ('Manager', 90000.25, 2),
   ('Manager', 75000.75, 3),
+  ('Manager', 80000.00, 4),
   ('Developer', 70000.36, 1),
   ('Developer', 60100.58, 2),
-  ('Customer Rep', 30000.22, 3);
+  ('Customer Rep', 30000.22, 3),
+  ('Recruiter', 30000.00, 4);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
   ('Daniel', 'Lunst', 1, NULL),
   ('Amber', 'Deter', 2, NULL),
   ('Oliver', 'Bolang', 3, NULL),
-  ('Scott', 'Quill', 4, 1),
-  ('Nick', 'Alling', 6, 3),
-  ('Maggie', 'Sipella', 5, 2),
-  ('Chris', 'Rotran', 5, 2),
-  ('Jordan', 'Zelle', 4, 1),
-  ('Silvia', 'Orta', 6, 3),
-  ('Arn', 'Hachem', 5, 2),
-  ('Zach', 'Asalam', 6, 3),
-  ('Tina', 'Wright', 4, 1);
+  ('Tina', 'Wright', 4, NULL),
+  ('Scott', 'Quill', 5, 1),
+  ('Nick', 'Alling', 6, 2),
+  ('Maggie', 'Sipella', 7, 3),
+  ('Chris', 'Rotran', 8, 4),
+  ('Jordan', 'Zelle', 5, 1),
+  ('Silvia', 'Orta', 6, 2),
+  ('Arn', 'Hachem', 7, 3),
+  ('Zach', 'Asalam', 8, 4);
